fddaa1d310775fb26657874a0aa9f0c28b2334dd
PrestonThomsen <pthomsen40@stuy.edu>
2024-05-22 14:41:38 -0400

    Setting up the main file

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+    char *filename = argv[0];
+    //We will have status changes come after the file which is being modified so that it is easier to incorporate new things as we get to it
+}
