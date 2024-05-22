a7964eef25295b22b71225209298f9e8bc6fea70
PrestonThomsen <pthomsen40@stuy.edu>
2024-05-22 14:46:47 -0400

    Setting up h files (I forgot what those are)

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "parser.h"
 
 int main(int argc, char *argv[]) {
     char *filename = argv[0];
     //We will have status changes come after the file which is being modified so that it is easier to incorporate new things as we get to it
+
 }
