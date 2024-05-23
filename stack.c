5adddbdba878af8ce951b88a7ae4fcd13144ca0f
PrestonThomsen <pthomsen40@stuy.edu>
2024-05-23 14:50:59 -0400

    Forgot to add files in last commit

---
+++ stack.c
@@ -0,0 +1,5 @@
+#include <stdio.h>  // FILE, fgets, fopen, printf
+#include <stdlib.h> // strol
+#include <string.h> // strncmp, strlen, strchr, strncpy, strcspn
+#include "stack.h" // Implementation of stacks into C
+
---
+++ stack.h
@@ -0,0 +1,10 @@
+#ifndef STACK_H
+#define STACK_H
+
+int push(double[] stack, char* ingredient);
+int pop(double[] stack, char* ingredient);
+char* peek(double[] stack, char* ingredient);
+int isFull(double[] stack);
+int isEmpty(double[] stack);
+
+#endif // STACK_H
