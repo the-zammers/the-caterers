54e1d8926ede88f3c468a1d828fb8389dc1801a3
PrestonThomsen <pthomsen40@stuy.edu>
2024-05-23 14:56:24 -0400

    All set up for stack implementation at home

--- stack.c
+++ stack.c
@@ -3,3 +3,22 @@
 #include <string.h> // strncmp, strlen, strchr, strncpy, strcspn
 #include "stack.h" // Implementation of stacks into C
 
+int push(double[] stack, double ingredient) {
+    
+}
+
+int pop(double[] stack, double ingredient) {
+
+}
+
+char* peek(double[] stack, double ingredient) {
+
+}
+
+int isFull(double[] stack) {
+
+}
+
+int isEmpty(double[] stack) {
+
+}
