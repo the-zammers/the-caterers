f8a956eee3772cab0c149827d786d46d646cbd8c
Z Cater <zcater40@stuy.edu>
2024-05-29 14:17:59 -0400

    Added switch-case statement during parsing

--- execute.c
+++ execute.c
@@ -8,38 +8,88 @@ void execute(struct Recipe recipe){
 
   for(int iptr=0; iptr<recipe.step_count; iptr++){
     struct Step *curr = recipe.steps + iptr;
+    struct Ingredient *ing = &(recipe.ingredients[curr->ingredient]);
 
     switch(curr->command){
       case INPUT:
-        printf("inputting");
+        printf("inputting\n");
+        printf("Input requested.\n> ");
+        scanf("%ld", &(ing->count));
         break;
 
       case PUSH:
-        printf("pushing");
+        printf("pushing\n");
+        break;
+
+      case POP:
+        printf("popping\n");
         break;
 
       case ADD:
-        printf("adding");
+        printf("adding\n");
+        break;
+
+      case ADD_MANY:
+        printf("adding many\n");
         break;
 
       case SUBTRACT:
-        printf("subtracting");
+        printf("subtracting\n");
+        break;
+
+      case MULTIPLY:
+        printf("multiplying\n");
+        break;
+
+      case DIVIDE:
+        printf("dividing\n");
         break;
 
       case GLYPH_MANY:
-        printf("glyphing many");
+        printf("glyphing many\n");
+        break;
+
+      case GLYPH:
+        printf("glyphing one\n");
+        ing->state = LIQUID;
+        break;
+
+      case RANDOMIZE:
+        printf("randomizing\n");
+        break;
+
+      case CLEAN:
+        printf("cleaning\n");
         break;
 
       case PRINT:
-        printf("printing");
+        printf("printing\n");
+        break;
+
+      case WHILE:
+        printf("whiling\n");
+        break;
+
+      case END:
+        printf("ending\n");
+        break;
+
+      case BREAK:
+        printf("breaking\n");
+        break;
+
+      case SUBROUTINE:
+        printf("subroutining\n");
+        break;
+
+      case RETURN:
+        printf("returning\n");
         break;
 
       default:
-        printf("invalid command (what)");
+        printf("invalid command (what)\n");
         break;
     }
-
-    printf("\n");
   }
 
 }
